0876deb51299301b821b89773c6aed680701fa9d
Joe Souch <p0091841@brookes.ac.uk>
2024-09-09 12:08:24 +0100

    Sync members in group

--- externallib.php
+++ externallib.php
@@ -28,141 +28,10 @@ require_once($CFG->dirroot.'/group/externallib.php');
 
 class local_obu_assessment_groups_external extends external_api {
 
-//    public static function get_course_groups_parameters() {
-//        return new external_function_parameters(
-//            array(
-//                'groupids' => new external_multiple_structure(new external_value(PARAM_INT, 'Group ID')
-//                    ,'List of group ids. A group id is an integer.'),
-//            )
-//        );
-//    }
-//
-//    public static function get_course_groups_returns() {
-//        return new external_multiple_structure(
-//            new external_single_structure(
-//                array(
-//                    'id' => new external_value(PARAM_INT, 'group record id'),
-//                    'courseid' => new external_value(PARAM_INT, 'id of course'),
-//                    'name' => new external_value(PARAM_TEXT, 'group name'),
-//                    'description' => new external_value(PARAM_RAW, 'group description text'),
-//                    'descriptionformat' => new external_format_value('description'),
-//                    'enrolmentkey' => new external_value(PARAM_RAW, 'group enrol secret phrase'),
-//                    'idnumber' => new external_value(PARAM_RAW, 'id number')
-//                )
-//            )
-//        );
-//    }
-//
-//    public static function get_course_groups($groupIds) {
-//        // Context validation
-//        self::validate_context(context_system::instance());
-//
-//        // Parameter validation
-//        self::validate_parameters(
-//            self::add_session_parameters(), array(
-//                'groupids' => $groupIds,
-//            )
-//        );
-//
-//        if (count($groupIds) == 0) {
-//            return array('result' => -1);
-//        }
-//
-//        if ($courseGroups = core_group_external::get_groups($groupIds)) {
-//            return $courseGroups;
-//        }
-//
-//        return array('result' => -9);
-//    }
-
-//    public static function get_group_members_parameters() {
-//        return new external_function_parameters(
-//            array(
-//                'groupids' => new external_multiple_structure(new external_value(PARAM_INT, 'Group ID')
-//                    ,'List of group ids. A group id is an integer.'),
-//            )
-//        );
-//    }
-//
-//    public static function get_group_members_returns() {
-//        return new external_multiple_structure(
-//            new external_single_structure(
-//                array(
-//                    'groupid' => new external_value(PARAM_INT, 'group record id'),
-//                    'userids' => new external_multiple_structure(new external_value(PARAM_INT, 'user id')),
-//                )
-//            )
-//        );
-//    }
-//
-//    public static function get_group_members($groupIds) {
-//        // Context validation
-//        self::validate_context(context_system::instance());
-//
-//        // Parameter validation
-//        self::validate_parameters(
-//            self::add_session_parameters(), array(
-//                'groupids' => $groupIds,
-//            )
-//        );
-//
-//        if (count($groupIds) == 0) {
-//            return array('result' => -1);
-//        }
-//
-//        if ($groupMembers = core_group_external::get_group_members($groupIds)) {
-//            return $groupMembers;
-//        }
-//
-//        return array('result' => -9);
-//    }
-
-//    public static function delete_group_members_parameters() {
-//        return new external_function_parameters(
-//            array(
-//                'members'=> new external_multiple_structure(
-//                    new external_single_structure(
-//                        array(
-//                            'groupid' => new external_value(PARAM_INT, 'group record id'),
-//                            'userid' => new external_value(PARAM_INT, 'user id'),
-//                        )
-//                    )
-//                )
-//            )
-//        );
-//    }
-//
-//    public static function delete_group_members_returns() {
-//        return new external_single_structure(
-//            array(
-//                'result' => new external_value(PARAM_INT, 'Result')
-//            )
-//        );
-//    }
-//
-//    public static function delete_group_members($members) {
-//        global $DB;
-//
-//        // Context validation
-//        self::validate_context(context_system::instance());
-//
-//        //TODO:: Param validation
-//
-//        $params = self::validate_parameters(self::delete_group_members_parameters(), array('members'=>$members));
-//
-//        foreach ($params['members'] as $member) {
-//            $groupid = $member['groupid'];
-//            $userid = $member['userid'];
-//
-//            groups_remove_member($groupid, $userid);
-//        }
-//
-//        return array('result' => 1);
-//    }
-
     public static function sync_group_members_parameters() {
         return new external_function_parameters(
             array(
+                'courseidnumber' => new external_value(PARAM_TEXT, 'course record id number'),
                 'groupidnumber' => new external_value(PARAM_TEXT, 'group record id number'),
                 'members' => new external_multiple_structure(
                     new external_single_structure(
@@ -183,18 +52,28 @@ class local_obu_assessment_groups_external extends external_api {
         );
     }
 
-    public static function sync_group_members($members)
+    public static function sync_group_members($courseidnumber, $groupidnumber, $members)
     {
         global $CFG, $DB;
-        require_once("$CFG->dirroot/group/lib.php");
 
-        $params = self::validate_parameters(self::sync_group_members_parameters(), array('members'=>$members));
+        require_once("$CFG->dirroot/grouplib.php");
+        require_once("$CFG->dirroot/grouplib.php");
 
-        $transaction = $DB->start_delegated_transaction();
+        $params = self::validate_parameters(self::sync_group_members_parameters(), array(
+            'courseidnumber' => $courseidnumber,
+            'groupidnumber' => $groupidnumber,
+            'members' => $members
+        ));
 
-        
+        if (!($course = $DB->get_record('course', array('idnumber' => $params['courseidnumber'])))) {
+            return array('result' => -1);
+        }
 
-        $transaction->allow_commit();
+        if (!($group = groups_get_group_by_idnumber($course->id, $params['groupidnumber']))) {
+            return array('result' => -2);
+        }
+
+        return local_obu_assessment_groups_sync_group_members($group, $members);
     }
 
 
--- locallib.php
+++ locallib.php
@@ -22,13 +22,59 @@
  * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
 
-require_once($CFG->dirroot."/grouplib.php");
+function local_obu_assessment_groups_sync_group_members($group, $members) {
+    global $CFG, $DB;
 
-function local_obu_assessment_groups_sync_group_members() {
+    require_once("$CFG->dirroot/grouplib.php");
 
+    $current_members = groups_get_members($group->id);
+
+    $current_member_usernames = array_map(function($user) {
+        return $user->username;
+    }, $current_members);
+
+    $members_to_add = array_diff($members, $current_member_usernames);
+    foreach($members_to_add as $username) {
+        local_obu_assessment_groups_sync_group_member_add($group, $username);
+    }
+
+    $members_to_delete = array_diff($current_member_usernames, $members);
+    foreach($members_to_delete as $username) {
+        local_obu_assessment_groups_sync_group_member_delete($group, $username);
+    }
+
+    return array(
+        'Result' => 1
+    );
+}
+
+function local_obu_assessment_groups_sync_group_member_add($group, $username) {
+    global $CFG, $DB;
+
+    require_once("$CFG->dirroot/grouplib.php");
+    require_once("$CFG->dirroot/local/obu_assessment_extensions/lib.php");
+
+    $user = $DB->get_record('user', array('username'=>$username), '*', MUST_EXIST);
+
+    groups_add_member($group, $user);
+
+    local_obu_assess_ex_recalculate_due_dates_for_user($user, $group);
+}
+
+function local_obu_assessment_groups_sync_group_member_delete($group, $username) {
+    global $CFG, $DB;
+
+    require_once("$CFG->dirroot/grouplib.php");
+
+    $user = $DB->get_record('user', array('username'=>$username), 'id', MUST_EXIST);
+
+    groups_remove_member($group, $user);
 }
 
 function local_obu_assessment_groups_create_group($course, $groupidnumber, $groupname) {
+    global $CFG;
+
+    require_once("$CFG->dirroot/local/obu_group_manager/lib.php");
 
     return local_obu_group_manager_create_system_group($course, $groupname, $groupidnumber);
 }
