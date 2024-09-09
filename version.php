de3499167cf086ccf58c71dac15b820fb48ada4c
Joe Souch <p0091841@brookes.ac.uk>
2024-09-09 14:11:24 +0100

    Define the API services to moodle

--- version.php
+++ version.php
@@ -23,7 +23,7 @@
  */
 defined('MOODLE_INTERNAL') || die();
 
-$plugin->version = 2024090901;
+$plugin->version = 2024090902;
 $plugin->requires = 2012120301;
 $plugin->component = 'local_obu_assessment_groups';
 $plugin->maturity = MATURITY_STABLE;
